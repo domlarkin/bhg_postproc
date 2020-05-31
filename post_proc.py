43e7cb6ee3e3784943b6ac17fbec23cce09b4e58
User1 Nuc42 <user1@nuc42.com>
2020-05-31 07:45:20 -0400

    transition to a computer

--- post_proc.py
+++ post_proc.py
@@ -29,7 +29,16 @@ def convert_time(filename):
     filename.split('_')
     return new_time    
     
-def make_csv(makeall = True): 
+def convert_bin_2_csv(makeall = True): 
+    '''
+    Converts an Ardupilot BIN file into a csv file. 
+    makeall if true converts all message types.
+    makeall if false converts only message types listed in the m_type list
+    The output is in the form of:
+    Timestamp epoch seconds, timestamp human readable, the message.
+    '''
+    # TODO accept log dir as parameter
+    # TODO walk the directory for all BIN files
     log_dir = '/home/user1/Downloads/APM_MAY27Flights/LOGS/'
     filename = log_dir + "00000010.BIN"
 
@@ -38,8 +47,7 @@ def make_csv(makeall = True):
     m_type=['GPA', 'GPS', 'AHR2', 'POS']
     mlog = mavutil.mavlink_connection(filename, planner_format,
                                       notimestamps,
-                                      robust_parsing=True)
-    
+                                      robust_parsing=True)    
     csv_filename = f'{log_dir}00000010.csv'
     if(not makeall):
         csv_filename = f'{log_dir}00000010_loc.csv'
@@ -60,11 +68,12 @@ def make_csv(makeall = True):
                 if notimestamps:
                     outstring = f'{m}'
                 else:
+                    # TODO format the CSV better
+                    #print(m.to_dict().keys())
                     outstring = "%s,%s.%02u,%s" % (m._timestamp,
                         time.strftime("%Y-%m-%d %H:%M:%S",
                                       time.localtime(m._timestamp)),
-                        int(m._timestamp*100.0)%100, m)    
-                print(outstring)
+                        int(m._timestamp*100.0)%100, m)
                 f.write(f'{outstring}\n')
 
 def make_datetime(name):
@@ -74,7 +83,7 @@ def make_datetime(name):
         m_sec = f'{m_sec}0'
     outstring=f'{fname_split[1][:4]}-{fname_split[1][4:6]}-{fname_split[1][6:8]} {fname_split[2][:2]}:{fname_split[2][2:4]}:{fname_split[2][4:6]}.{m_sec}'
     
-    print(outstring) # 2020-05-28 18-02-08.44
+    #print(outstring) # 2020-05-28 18-02-08.44
     utc_time = datetime.strptime(outstring, "%Y-%m-%d %H:%M:%S.%f")
     epoch_time = (utc_time - datetime(1970, 1, 1)).total_seconds()
     return (epoch_time,outstring)
@@ -82,17 +91,18 @@ def make_datetime(name):
 if __name__ == '__main__':
     rootDir = set_datadir() + '20200528_180208_387820/'
     rootDir = '/home/user1/Data/20200528_180208_387820/GOBI000088'
-#    make_csv()
+    #convert_bin_2_csv()
+    #convert_bin_2_csv(False)
+    exit()
       
     for dirName, subdirList, fileList in os.walk(rootDir):
         fileList = sorted(fileList)
         print('Found directory: %s' % dirName)
         for fname in fileList:
-            #print(f'{fname}')
-          
+            #print(f'{fname}')          
             if ('GOBI' in fname):
-                e_time, d_string = make_datetime(fname)
-                print(f'\t{fname}  date {d_string}  etime: {e_time}')
+                file_etime, file_stime = make_datetime(fname)
+                print(f'\t{fname}  date {file_stime}  etime: {file_etime}')
 
 
 
