b4931e8a54d9daafc7553784379c65c10c3643db
User1 Nuc01 <user1@nuc01.com>
2022-02-11 00:33:37 -0500

    in infinite loop, some cleanup done

--- epost_proc_PC21.py
+++ epost_proc_PC21.py
@@ -28,7 +28,7 @@ def set_datadir():
     print("===== The data directory is: %s =====" % data_dir) 
     return data_dir    
 
-# Return the start and end times of a log file
+# Return the start and end times of an ardupilot log file (BIN file)
 def get_bin_times(filename):
     notimestamps = False
     planner_format = True
@@ -48,7 +48,8 @@ def get_bin_times(filename):
             stop_time = m._timestamp
     return (start_time,stop_time)
 
-# Create a csv file that has the start and stop times of each log file in this directory    
+# Create a string with the start and stop times of each ardupilot log file (BIN file) in this directory 
+# Write the string to file and return the string   
 def make_bin_times_csv(bin_dir):
     binfile_list = []
     for dirName, subdirList, fileList in os.walk(bin_dir):
@@ -56,7 +57,7 @@ def make_bin_times_csv(bin_dir):
             bin_filename = os.path.join(dirName,fname)
             if bin_filename.endswith('BIN'):
                 binfile_list.append(bin_filename)
-                
+
     binfile_list.sort()  
     csv_filename = bin_dir + "/logfile_times.csv"
     with open(csv_filename, 'w') as csvfile:    
@@ -72,9 +73,11 @@ def make_bin_times_csv(bin_dir):
                                       time.gmtime(end_time)),
                                       int(end_time*100.0)%100)                              
 
-            outstring = f"{bin_filename.split('/')[-1]},{begin_time},{end_time},{human_begin},{human_end}"
-            print(outstring)
+            #outstring += f"{bin_filename.split('/')[-1]},{begin_time},{end_time},{human_begin},{human_end}\n"
+            outstring = f"{bin_filename},{begin_time},{end_time},{human_begin},{human_end}"
             csvfile.write(outstring+'\n')
+            print(outstring)
+    return (csv_filename)
 
 def convert_bin(filename): 
     '''
@@ -139,6 +142,7 @@ def get_datetime_filename(name):
     datetime_object = datetime.strptime(sp_time, '%Y%m%d_%H%M%S_%f')
     return datetime_object
 
+# Return the closest item to a pivot
 def nearest(items, pivot):
     return min(items, key=lambda x: abs(x - pivot))
 
@@ -174,28 +178,16 @@ def make_st_datetime(str_time):
     return datetime_object
     
 # For Ardupilot message details see: https://ardupilot.org/plane/docs/logmessages.html    
-def get_flightbin_json(filename):
-#    csv_file = '/home/user1/Data/20220125_191846_355/20220125_191846.csv'  
-  
-    csv_file = '/home/user1/Data/20220125_181032_725/20220125_181032.csv'
-#    csv_file = '/home/user1/Data/20220125_191846_355/20220125_191846.csv'
-    line = get_flight_times(csv_file) # ('20220125_191846_971', '20220125_192737_914')
-    s_time = make_st_datetime(line[0])
-    e_time = make_st_datetime(line[1])
-    print(f"CSV File start time: {s_time}, end time: {e_time} and log file {filename}")
-
+def get_flightbin_json(bin_times_file, csv_file):
     notimestamps = False
     planner_format = True
     m_type=['AHR2']
-    mlog = mavutil.mavlink_connection(filename, planner_format,
+    mlog = mavutil.mavlink_connection(bin_times_file, planner_format,
                                       notimestamps,
                                       robust_parsing=True) 
-
-#    in_filename='/home/user1/Data/20220125_181032_725/20220125_181032.csv'
     csv_entries=[]
     with open(csv_file, 'r') as f:
         csv_entries=f.readlines()  
-
     csv_entries = csv_entries[5:]
 
     test_csvtime = csv_entries.pop(0).split(',')[1]
@@ -210,9 +202,6 @@ def get_flightbin_json(filename):
         m_dict=m.to_dict() 
         print(m._timestamp)
         log_dt = datetime.utcfromtimestamp(m._timestamp) # Convert to a datetime object
-
-        if (log_dt < s_time) or (log_dt > e_time): # Skip processing if log entry is before the flight begins
-            continue
 
         # Add time stamps to the dictionary. All times are in UTC.
         human_time = "%s.%02u" % (time.strftime("%Y-%m-%d %H:%M:%S",
@@ -276,25 +265,79 @@ def parse_ahr2(last_msg, crnt_msg,csv_time):
         print("\nFound it =====   {:.3f}  ALTS:{:.3f}, {:.3f}, {:.3f}, {:.3f}".format(csv_time,last_msg['Alt'],return_msg['Alt'],crnt_msg['Alt'],pct))
     return return_msg
 
-if __name__ == '__main__':
-#    rootDir = set_datadir() + '20200528_180208_387820/'
-#    rootDir = '/home/user1/DATA_ARCHIVE/BHGTest'
-#    rootDir = set_datadir()
-    binlog_dir = '/home/user1/Desktop/Yuma202201HarveyLogs/APM/LOGS'
-#    csv_file = '/home/user1/Data/20220131_185934_917/20220131_185934.csv'
-#    csv_file = '/home/user1/Data/20220125_191846_355/20220125_191846.csv'
+# Find and delete any empty files. Sometimes the code creates empty image files.
+def remove_empty_files(top_dir):
+    empty_files = []
+    for dirName, subdirList, fileList in os.walk(top_dir):
+        for fname in fileList:
+            full_filename = os.path.join(dirName,fname)
+            if (os.path.getsize(full_filename) < 1):
+                empty_files.append(full_filename)
+                print("Removing empty file at: %s" %full_filename)  
+                os.remove(full_filename)
+    return empty_files
 
-    bin_filename = f'{binlog_dir}/00000077.BIN'
-    get_flightbin_json(bin_filename)
+# Create a list of empty directories.
+def delete_empty_folders(dir_path):
+    dirs_to_delete = []
+    for root, dirs, files in os.walk(dir_path, topdown=False):
+        for dirName in dirs:
+            full_filename = os.path.join(root,dirName)
 
+            num_files = sum([len(files) for r, d, files in os.walk(full_filename)])
+            #print(full_filename,num_files)
+            if (num_files < 3):
+                dirs_to_delete.append(full_filename)
+                print("++++++ DELETING UNUSED FOLDER %s +++++++" % full_filename)
+                shutil.rmtree(full_filename) 
+    return dirs_to_delete
 
+def process_flights(bin_times_file, data_dir):
+    #1 Walk data directory and process each directory
+    for dirName, subdirList, fileList in os.walk(data_dir):
+        for fname in fileList:
+            full_filename = os.path.join(dirName,fname)
+            if full_filename.endswith('csv'):
+                bin_file = get_bin_file(full_filename, bin_times_file)
+                
+def get_bin_file(filename, bin_times_file):
+    (start_t,stop_t) = get_flight_times(filename)        
+    utc_time = datetime.strptime(start_t, "%Y%m%d_%H%M%S_%f")
+    s_time = (utc_time - datetime(1970, 1, 1)).total_seconds()     
+    utc_time = datetime.strptime(stop_t, "%Y%m%d_%H%M%S_%f")
+    e_time = (utc_time - datetime(1970, 1, 1)).total_seconds()
     
-    #make_bin_times_csv(binlog_dir)
+    with open(bin_times_file, 'r') as f:
+        times = f.readlines() 
+        
+    for item in times:
+#        print(s_time,e_time,item)
+        item_list = item.split(',')
+        if s_time > float(item_list[1]) and e_time < float(item_list[2]):
+            print(f"=====\nUsing {item_list[0]} to process {filename}.") 
+            get_flightbin_json(item_list[0],filename)
+            break       
+    
+
+if __name__ == '__main__':
+
+    # 1. Walk directories and remove any empty files
+
+    # 2. Walk directories and remove empty directories
+
+    # 3. Create a csv file with a list of ardupilot BIN file start and stop times.
+    binlog_dir = '/home/user1/Desktop/Yuma202201HarveyLogs/APM/LOGS/'
+#    bin_times_csv_filename = make_bin_times_csv(binlog_dir) # Only need to run this once
+    bin_times_csv_filename = binlog_dir + 'logfile_times.csv' # after the above line runs I use this one.
+
+    # 4. Walk directory and find flight csv file and interpolate position data and create json of BIN file data for this flight
+#    flight_dir = '/home/user1/Data/20220131_185934_917/'
+    flight_dir = '/home/user1/Data/20220125_191846_355/'
+    process_flights(bin_times_csv_filename,flight_dir)
 
 
 
 '''
-
         # {'mavpackettype': 'AHR2', 'TimeUS': 618102902, 'Roll': -1.04, 'Pitch': -6.8500000000000005, 'Yaw': 258.05, 'Alt': 165.41000366210938, 'Lat': 32.8470934, 'Lng': -114.2651712, 'Q1': 0.6281170845031738, 'Q2': -0.052161648869514465, 'Q3': -0.030532440170645714, 'Q4': -0.7757678627967834, 'TimeStamp': 1643138757.445591, 'HumanTime': '2022-01-25 19:25:57.44'}
 
 '''
